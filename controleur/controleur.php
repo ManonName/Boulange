bff52ea0413e979ac6e9a2a3d296f1172638141f
Manon Naamane <mn.naamane@gmail.com>
2022-05-28 10:47:29 +0200

    Ajout du routeur et du controlleur avec des echos pour chaque routes

--- controleur/controleur.php
+++ controleur/controleur.php
@@ -1,8 +1,53 @@
 <?php
 require_once "./outil/outils.php";
-require_once "./model/OrdiManager.php";
+
 
 function afficherAccueil(){
     require "vue/accueil.php";
 }
+
+function afficherOrdis(){
+    echo "on est dans afficherOrdis.";
+}
+
+function supprimerOrdi($id){
+    echo "On est dans supprimerOrdi.";
+}
+
+function afficherOrdi($id){
+    echo "On est dans afficherOrdi.";
+}
+
+function creerOrdi(){
+    echo "On est dans creerOrdi.";
+}
+
+function creerValidationOrdi(){
+    echo "On est dans creerValidationOrdi.";
+}
+
+function afficherCardOrdis(){
+    echo "On est dans afficherCardOrdis";
+}
+
+function modifierOrdi($id){
+    echo "On est dans modifierOrdi";
+}
+
+function modifierValidationOrdi(){
+    echo "On est dans modifierValidationOrdi.";
+}
+
+function ajouterOrdiPanier($id){
+    echo "On est dans ajouterOrdiPanier.";
+}
+
+function afficherCommande(){
+    echo "On est dans afficherCommande.";
+}
+
+function supprimerCommande(){
+    echo "On est dans supprimerCommande";
+}
+
 ?>
