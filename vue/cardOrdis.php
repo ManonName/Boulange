5ff29056a75ed80938358c6fdf6d8e6090d03488
Manon Naamane <mn.naamane@gmail.com>
2022-05-30 11:22:10 +0200

    Ajouter sur les cartes l'action détail qui permet d'afficher les détails du livre sélectionné

--- vue/cardOrdis.php
+++ vue/cardOrdis.php
@@ -10,10 +10,10 @@
           <img height="300px" src="public/images/<?php echo $ordi['image']; ?>" class="card-img-top" alt="image">
           <div class="card-body">
             <h5 class="card-title"><?php echo sousChaineTaille($ordi['denomination'], 18); ?></h5>
-            <p class="card-text"><?php echo sousChaineTaille($ordi['processeur'],50); ?></p>
-            <p class="card-text"><?php echo sousChaineTaille($ordi['prix'],50); ?></p>
-            <p class="card-text"><?php echo sousChaineTaille($ordi['ecran'],50); ?></p>
-            <p class="card-text"><?php echo sousChaineTaille($ordi['vive'],50); ?></p>
+            <p class="card-text">Processeur : <?php echo sousChaineTaille($ordi['processeur'],50); ?></p>
+            <p class="card-text">Prix : <?php echo sousChaineTaille($ordi['prix'],50); ?></p>
+            <p class="card-text">Taille d'écran : <?php echo sousChaineTaille($ordi['ecran'],50); ?></p>
+            <p class="card-text">Mémoire vive : <?php echo sousChaineTaille($ordi['vive'],50); ?></p>
 
             <a href="index.php?action=lire&id=<?php echo $ordi['id']; ?>" class="btn btn-primary">Détail</a>
             <a href="index.php?action=addpanier&id=<?php echo $ordi['id']; ?>" class="btn btn-success">Panier</a>
